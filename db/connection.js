e29479aecf8d44425ec4bc1423cbda10698ff2d3
Hannah Aurand <aurand.hannah@gmail.com>
2018-04-16 11:17:20 -0400

    changing back to quotes

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = Promise
 
-if (process.env.NODE_ENV === production) {
+if (process.env.NODE_ENV === 'production') {
   mongoose.connect(process.env.MLAB_URL)
 } else {
   mongoose.connect('mongodb://localhost/barcelona-back-end')
